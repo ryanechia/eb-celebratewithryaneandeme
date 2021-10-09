ca8964b9c803a1dc3be07ba89dee2535b155c537
Ryane Chia <ryanechia@outlook.com>
2021-10-10 02:34:02 +0800

    responsive classes for banner text positions

--- wedding-fe/src/app/home/home.module.ts
+++ wedding-fe/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,7 +16,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    MatButtonModule
+    MatButtonModule,
+    FlexLayoutModule
   ]
 })
 export class HomeModule { }
