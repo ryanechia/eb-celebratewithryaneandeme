e6bb655a94444f8257ba8faa1524b7e3a1c344f7
Ryane Chia <ryanechia@outlook.com>
2021-10-10 00:47:49 +0800

    implement responsive menu bar

--- wedding-fe/src/app/app.module.ts
+++ wedding-fe/src/app/app.module.ts
@@ -4,6 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule, FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -13,8 +20,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    // material modules
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatToolbarModule,
+    MatSidenavModule,
+
+    // material flex
+    FlexModule,
+    FlexLayoutModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
