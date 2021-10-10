4c0916d857e8932fb09105363b2619c96b3bd8c8
Ryane Chia <ryanechia@outlook.com>
2021-10-10 17:01:37 +0800

    mobile responsive stlying for date time countdown

--- wedding-fe/src/app/utils/utils.module.ts
+++ wedding-fe/src/app/utils/utils.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DateTimeCountdownComponent } from './date-time-countdown/date-time-countdown.component';
+import { ExtendedModule } from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [ DateTimeCountdownComponent ],
   imports: [
-    CommonModule
+    CommonModule,
+    ExtendedModule
   ],
   exports: [
     DateTimeCountdownComponent
