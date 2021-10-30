5f34e329ef0754bc8fc48ee015129a668c21e37b
Ryane Chia <ryanechia@outlook.com>
2021-10-30 17:57:57 +0800

    use gallerize to show all images in modal carousel

--- wedding-fe/src/app/gallery/gallery.component.ts
+++ wedding-fe/src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-gallery',
@@ -6,8 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-
-  constructor() { }
+  images: GalleryItem[];
+  constructor() {
+    this.images = [
+      new ImageItem({ src: 'assets/photos/IMG_3927.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_3927.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_3984.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_3984.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4116.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4116.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4118.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4118.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4144.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4144.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4153.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4153.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4231.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4231.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4245.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4245.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4251.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4251.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4271.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4271.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4286.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4286.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4337.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4337.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4386.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4386.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4389.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4389.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4473.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4473.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4487.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4487.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4503.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4503.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4511.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4511.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4594.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4594.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4638.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4638.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4700.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4700.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4716.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4716.jpeg' }),
+      new ImageItem({ src: 'assets/photos/IMG_4738.jpg', thumb: 'assets/photos/thumbnails/THUMB_IMG_4738.jpeg' }),
+    ];
+  }
 
   ngOnInit(): void {
   }
--- wedding-fe/src/app/gallery/gallery.module.ts
+++ wedding-fe/src/app/gallery/gallery.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryComponent } from './gallery.component';
 import { RouterModule, Routes } from '@angular/router';
+import { GalleryModule as NgGalleryModule } from 'ng-gallery';
+import { LightboxModule } from 'ng-gallery/lightbox';
 
 const routes: Routes = [
   { path: '', component: GalleryComponent },
@@ -13,7 +15,9 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    NgGalleryModule,
+    LightboxModule
   ]
 })
 export class GalleryModule { }
